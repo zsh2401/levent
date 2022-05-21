5c85b2c3e8f31e3c438f2f4450956e5de449c34b
zsh2401 <zsh2401@163.com>
2022-05-21 14:35:18 +0800

    umd support

--- esbuild.ts
+++ esbuild.ts
@@ -1,13 +1,13 @@
 import esbuild from "esbuild"
 import { resolve } from "path"
 esbuild.build({
-    entryPoints: [resolve(__dirname, "src/index.ts")],
+    entryPoints: [resolve(__dirname, "src/umd.ts")],
     bundle: true,
     minify: true,
     outfile: resolve(__dirname, "dist/eventx.min.js")
 })
 esbuild.build({
-    entryPoints: [resolve(__dirname, "src/index.ts")],
+    entryPoints: [resolve(__dirname, "src/umd.ts")],
     bundle: true,
     sourcemap: true,
     outfile: resolve(__dirname, "dist/eventx.js")
